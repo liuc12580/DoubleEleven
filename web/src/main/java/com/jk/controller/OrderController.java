68d5cfd34578148b60dbe81a08dba0e0123b1212
liuc12580 <958616386@qq.com>
2020-10-27 11:31:30 +0800

    添加config和bus

--- web/src/main/java/com/jk/controller/OrderController.java
+++ web/src/main/java/com/jk/controller/OrderController.java
@@ -1,17 +1,39 @@
 package com.jk.controller;
 
+import com.jk.config.RedisConstant;
 import com.jk.service.OrderService;
+import com.jk.utils.RedisUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@RefreshScope
 @RestController
 public class OrderController {
 
     @Resource
+    private RedisUtil redisUtil;
+
+    // 读取配置文件的信息
+    @Value("${user.name}")
+    private String name;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Resource
     private OrderService orderService;
+
+    @RequestMapping("/getUserName")
+    public String getUserName() {
+        System.out.println(name + "--" + port);
+//        Object userName = redisUtil.get(RedisConstant.GET_USER_NAME);
+        return name + "--" + port;
+    }
 
     @RequestMapping("/select")
     @HystrixCommand(fallbackMethod = "selectFail")
